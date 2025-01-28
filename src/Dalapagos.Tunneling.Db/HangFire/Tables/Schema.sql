49706e5280b4cec2f1e02ec519f26e2ffeef70b0
Paul Rutherford <paul_rutherford@live.com>
2025-01-28 07:55:11 -0700

    scripts added

--- src/Dalapagos.Tunneling.Db/HangFire/Tables/Schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE [HangFire].[Schema] (
-    [Version] INT NOT NULL,
-    CONSTRAINT [PK_HangFire_Schema] PRIMARY KEY CLUSTERED ([Version] ASC)
-);
-
-
-GO
-
--- src/Dalapagos.Tunneling.Db/Security/HangFire.sql
+++
@@ -1,6 +0,0 @@
-CREATE SCHEMA [HangFire]
-    AUTHORIZATION [dbo];
-
-
-GO
-
